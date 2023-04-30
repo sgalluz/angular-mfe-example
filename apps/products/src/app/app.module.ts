d78055b08076c7f8a8c04fb1a33eb00e90a53bfa
Stefano Galluzzi <sgalluz@users.noreply.github.com>
2023-04-30 22:19:13 +0200

    Product list component template

--- apps/products/src/app/app.module.ts
+++ apps/products/src/app/app.module.ts
@@ -1,17 +1,39 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { appRoutes } from './app.routes';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import { ProductListComponent } from './pages/product-list/product-list.component';
+import { ProductDetailComponent } from './pages/product-detail/product-detail.component';
+import {
+  FaIconLibrary,
+  FontAwesomeModule,
+} from '@fortawesome/angular-fontawesome';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
+  declarations: [
+    AppComponent,
+    ProductListComponent,
+    ProductDetailComponent
+  ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledBlocking' }),
+    BrowserAnimationsModule,
+    CommonModule,
+    RouterModule.forRoot(appRoutes, {
+      initialNavigation: 'enabledBlocking',
+      useHash: true
+    }),
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(library: FaIconLibrary) {
+    library.addIconPacks(fas);
+  }
+}
--- apps/products/src/app/app.routes.ts
+++ apps/products/src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Route } from '@angular/router';
+import { ProductListComponent } from './pages/product-list/product-list.component';
+import { ProductDetailComponent } from './pages/product-detail/product-detail.component';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+    {
+        path: 'all',
+        component: ProductListComponent
+    },
+    {
+        path: ':id/detail',
+        component: ProductDetailComponent
+    },
+    {
+        path: '**',
+        pathMatch: 'full',
+        redirectTo: 'all'
+    }
+];
