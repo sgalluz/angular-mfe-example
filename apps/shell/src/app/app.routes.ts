3c5e3fbb3996275fc859e7dd37cbb160f4baf1ef
Stefano Galluzzi <sgalluz@users.noreply.github.com>
2023-05-01 13:35:03 +0200

    Products mfe draft inclusion (no use of manifest)

--- apps/shell/src/app/app.routes.ts
+++ apps/shell/src/app/app.routes.ts
@@ -1,11 +1,22 @@
 import { Route } from "@angular/router";
 import { HomeComponent } from "./pages/home/home.component";
 import { ContactsComponent } from "./pages/contacts/contacts.component";
+import { loadRemoteModule } from '@angular-architects/module-federation';
 
 export const appRoutes: Route[] = [
     {
         path: 'home',
         component: HomeComponent
+    },
+    {
+        path: 'products',
+        loadChildren: () =>
+            loadRemoteModule({
+                type: 'module',
+                remoteEntry: 'http://localhost:4210/remoteEntry.js',
+                exposedModule: './ProductsModule'
+            })
+            .then(m => m.ProductsModule)
     },
     {
         path: 'contacts',
